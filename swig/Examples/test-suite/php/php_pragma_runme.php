a252e3509d8ffb6dd8f67749faa6ae70b772ec5f
David Barbera <davidbarbera9@gmail.com>
2021-10-18 13:58:56 +0100

    Deleted unnecessary swig source code.

--- swig/Examples/test-suite/php/php_pragma_runme.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require "tests.php";
-
-check::equal('1.5',(new ReflectionExtension('php_pragma'))->getVersion(),"1.5==version(php_pragma)");
-
-check::done();
--- swig/Examples/test-suite/php/virtual_vs_nonvirtual_base_runme.php
+++
@@ -1,8 +0,0 @@
-<? 
-
-require "tests.php";
-
-$fail = new SimpleClassFail();
-$work = new SimpleClassWork();
-
-check::equal($work->getInner()->get(), $fail->getInner()->get(), "should both be 10");
